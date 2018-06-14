f82cf5612cea96be21775e501dcc9ee26630dd5d
surenderhsr44 <surenderhsr44@gmail.com>
2018-06-14 23:10:50 +0530

    first page add

---
+++ firstproject/src/firstproject/com/Surender.java
@@ -0,0 +1,19 @@
+package firstproject.com;
+
+public class Surender {
+
+	public static void main(String[] args) {
+		System.out.println("new Java 8 features ");
+		System.out.println("forEach() method in Iterable interface. ");
+		System.out.println(" default and static methods in Interfaces.");
+		System.out.println(" Functional Interfaces and Lambda Expressions.");
+		System.out.println("Java Stream API for Bulk Data Operations on Collections.\r\n" + 
+				"Java Time API.\r\n" + 
+				"Collection API improvements.\r\n" + 
+				"Concurrency API improvements.\r\n" + 
+				"Java IO improvements. ");
+		
+
+	}
+
+}
